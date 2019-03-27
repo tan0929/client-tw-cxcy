fb28adb11b478efc008d9cafe72b1302baeb36f3
JEN-HAO TAN <tan0929@outlook.com>
2019-03-27 14:44:28 -0700

    install styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `jt-starter`,
+    author: `James Tan <contact@jamestan.me>`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
