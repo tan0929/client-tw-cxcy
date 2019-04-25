8a59b934964ee9b011570b43a14b096512db455a
James Tan <tan0929@outlook.com>
2019-04-25 02:02:19 -0700

    remove facebookmessage for test

--- src/components/facebookMessage.js
+++ src/components/facebookMessage.js
@@ -1,22 +1,22 @@
 // import React from 'react';
 import breakpoint from 'styled-components-breakpoint';
-// import { FacebookProvider, SendToMessenger, MessageUs, Like, Page } from 'react-facebook';
 import styled from 'styled-components';
 
 import React, { Component} from 'react';
-import { FacebookProvider, SendToMessenger, EmbeddedPost, Page, MessageUs } from 'react-facebook';
 
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+// import MessengerCustomerChat from 'react-messenger-customer-chat';
 
-export default class FacebookMessage extends Component {
-  render() {
-    return (
-        <MessengerCustomerChat
-            pageId="2301403163467351"
-            appId="388611788532974"
-        />
-    );
-  }
-}
+// export default class FacebookMessage extends Component {
+//   render() {
+//     return (
+//         <MessengerCustomerChat
+//             pageId="2301403163467351"
+//             appId="388611788532974"
+//         />
+//     );
+//   }
+// }
+
+export default <div></div>
 
 
